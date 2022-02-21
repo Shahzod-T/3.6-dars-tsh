0f1ab84f0004e13ef33d3f8071eee5134ffecb51
Shahzod-T <shahzodbek299@gmail.com>
2022-02-21 22:23:37 +0500

    initial commit

---
+++ js/maim.js
@@ -0,0 +1,19 @@
+
+
+var Summa = prompt ("Assalomu alaykum Iltimos Pulni Kiriting" , 0);
+
+var a = Number (500)
+var b = Number (250)
+var e = Number (120)
+
+var a = (a * 10900)
+var b = (b * 10900)
+var e = (e * 12272)
+
+var all = Number((a + b + e ))
+
+if( Summa >= all ){
+    console.log("Alisher aka Oq Yo'l");
+}else{
+    console.log("Kechirasiz aka Mablag' Yetarlicha emas");
+}
